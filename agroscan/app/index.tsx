22b7a8e5908767f73ddfe7e5142a09e9d37fccc1
Fermin Cardenas <fermin.cadenas.cabal@gmail.com>
2025-03-30 23:22:25 -0600

    Cambios de version

--- agroscan/app/index.tsx
+++
@@ -1,7 +0,0 @@
-// app/index.tsx
-import { Redirect } from 'expo-router';
-
-export default function AppIndex() {
-  // Redirect to the Login screen by default
-  return <Redirect href="./Login/LoginScreen" />;
-}
